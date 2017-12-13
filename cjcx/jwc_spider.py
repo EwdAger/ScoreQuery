40d4dbe434ebc43e657a0e80fde9a2638a13c7c7
EwdAger <ewdager@hotmail.com>
2017-12-13 10:35:42 +0800

    V1.0 Update

--- cjcx/jwc_spider.py
+++ cjcx/jwc_spider.py
@@ -1,113 +1,77 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 import requests
-from PIL import Image
-from lxml import etree
+from bs4 import BeautifulSoup
+from captcha_verify import verify
 
 class spider(object):
 
-    def split_captcha(self):
-        im = Image.open('cjcx/yzm/captcha.png')
-        im = im.crop((215, 321, 295, 360))
-        im.save('cjcx/yzm/captcha.png')
-        im.show()
-
-    def login(self):
-        Path = r'D:\project\ScoreQuery\ScoreQuery\phantomjs-2.1.1-windows\bin\phantomjs.exe'
-        wd = webdriver.PhantomJS(executable_path=Path)
-        loginUrl = 'http://kdjw.hnust.cn/kdjw/'
-        wd.get(loginUrl)
-        wd.save_screenshot('cjcx/yzm/captcha.png')
-        self.split_captcha()
-        wd.find_element_by_xpath('//*[@id="userAccount"]').send_keys('1505020114')
-        wd.find_element_by_xpath('//*[@id="userPassword"]').send_keys('140216')
-        wd.find_element_by_xpath('//*[@id="RANDOMCODE"]').send_keys(raw_input("输入验证码："))
-        wd.find_element_by_xpath('//*[@id="imgbtn"]').click() #点击登陆
-
-        req = requests.Session()
-        cookies = wd.get_cookies()
-        for cookie in cookies:
-            req.cookies.set(cookie['name'], cookie['value'])
-        html1 = req.get('http://kdjw.hnust.cn/kdjw/xszqcjglAction.do?method=queryxscj')
-        html2 = req.get('http://kdjw.hnust.cn/kdjw/xszqcjglAction.do?method=toXfjdList')
-        wd.quit()
-        return [html1.text, html2.text]
-
-    def class_select(self, num):
-        major = ''
-        if num == "1":
-            major = "计算机科学与工程"
-        elif num == "2":
-            major = "网络工程"
-        elif num == "3":
-            major = "信息安全"
-        elif num == "4":
-            major = "物联网工程"
-        elif num == "5" or num == "6":
-            major = "软件工程"
-        return major
-
-    def query_spider(self):
-        html = self.login()
-        selector = etree.HTML(html[0])
-        selector2 = etree.HTML(html[1])
-        i, j = 1, 1
-        data_list = []
-        new_data_list = []
-
-        #成绩爬虫
-        while True:
-            block1 = selector.xpath('//tr[@id="' + str(i) + '"]')
-            if len(block1) != 0:
-                id = selector.xpath('//tr[@id="' + str(i) + '"]/td[3]/text()')
-                stu_id = id[0]
-                name = selector.xpath('//tr[@id="' + str(i) + '"]/td[4]/text()')
-                term = selector.xpath('//tr[@id="' + str(i) + '"]/td[5]/text()')
-                c_name = selector.xpath('//tr[@id="' + str(i) + '"]/td[6]/text()')
-                score = selector.xpath('//tr[@id="' + str(i) + '"]/td[7]/text()')
-                c_type = selector.xpath('//tr[@id="' + str(i) + '"]/td[9]/text()')
-                credit = selector.xpath('//tr[@id="' + str(i) + '"]/td[12]/text()')
-                properties = selector.xpath('//tr[@id="' + str(i) + '"]/td[13]/text()')
-                grade = stu_id[0:2] + "级"
-                major = self.class_select(stu_id[5])
-                class_num = stu_id[7] + "班"
-
-                data = {'stu_id': stu_id, 'name': name[0], 'term': term[0],
-                           'c_name': c_name[0], 'score': score[0], 'c_type': c_type[0],
-                           'credit': credit[0], 'properties': properties[0], 'grade': grade,
-                           'major': major, 'class_num': class_num}
-
-                data_list.append(data)
-                i += 1
-            else:
+    def login(self, username, password):
+        login_url = 'http://kdjw.hnust.cn/kdjw/Logon.do?method=logon'
+        check = "<script language=\"javascript\">window.location.href='http://kdjw.hnust.cn/kdjw/framework/main.jsp';</script>"
+        i = 0
+        msg = ''
+        while (True):
+            if i == 10:
+                msg = "学号密码错误或连接教务网失败"
                 break
-
-        #学分绩点爬虫
-        while True:
-            block2 = selector2.xpath('//tr[@id="' + str(j) + '"]')
-            if len(block2) != 0:
-                c_name = selector2.xpath('//tr[@id="' + str(j) + '"]/td[3]/text()')
-                point = selector2.xpath('//tr[@id="' + str(j) + '"]/td[6]/text()')
-                grade_point = selector2.xpath('//tr[@id="' + str(j) + '"]/td[7]/text()')
-                for data in data_list:
-                    if data['c_name'] == c_name[0] and data['properties'] == u'重修':
-                        data['point'] = ""
-                        data['grade_point'] = ""
-                        new_data_list.append(data)
-                    elif data['c_name'] == c_name[0]:
-                        data['point'] = point[0]
-                        data['grade_point'] = grade_point[0]
-                        new_data_list.append(data)
-                j += 1
-            else:
+            cookies = requests.get('http://kdjw.hnust.cn/kdjw/verifycode.servlet').cookies
+            img = requests.get('http://kdjw.hnust.cn/kdjw/verifycode.servlet', cookies=cookies).content
+            with open('cjcx/yzm/captcha.bmp', 'wb') as f:
+                f.write(img)
+            code = verify("cjcx/yzm/captcha.bmp")
+            data = {
+                'USERNAME': username,
+                'PASSWORD': password,
+                'RANDOMCODE': code
+            }
+            login = requests.post(login_url, cookies=cookies, data=data)
+            soup = BeautifulSoup(login.content, "lxml")
+            if str(soup.script) == check:
                 break
-        return new_data_list
+            i += 1
+
+        return cookies, msg
+
+    def score_spider(self, username, password):
+        cookies, msg = self.login(username, password)
+        if msg == '':
+            html = requests.get('http://kdjw.hnust.cn/kdjw/xszqcjglAction.do?method=queryxscj', cookies=cookies)
+            with open('cjcx/page/data.html', 'w') as f:
+                f.write(html.content)
+        return msg
+
+
+    def score_data(self):
+        soup = BeautifulSoup(open("cjcx/page/data.html"), "lxml")
+        keys = []
+        data = []
+        year = []
+
+        cj_soup = soup.find("tr", class_="smartTr")
+        while(True):
+            if cj_soup == None:
+                break
+            for i in range(4, 7):
+                key = cj_soup.contents[i].text.strip()
+                if i == 6 and key == '':
+                    key = '无成绩'
+                keys.append(key)
+            keys.append(cj_soup.contents[11].text.strip())
+            if not keys[0] in year:
+                y = keys[0]
+                year.append(y)
+            data.append(keys)
+            keys = []
+            cj_soup = cj_soup.next_sibling
+
+        return data, year
 
 
 """
 a = spider()
-b = a.query_spider()
-c = b[0]
-print c['c_name'], c['grade_point']
-print len(b)
+data = a.score_bs()
+
+for i in data:
+    for j in range(4):
+        print(i[j])
 """
--- cjcx/views.py
+++ cjcx/views.py
@@ -3,26 +3,26 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-import models
 import jwc_spider
-
+import json
 # Create your views here.
 
-def w_data():
+def login(request):
+    return render(request, 'login.html')
+
+def check(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
     spider = jwc_spider.spider()
-    data_list = spider.query_spider()      #返回字典列表
-    for data in data_list:
-        if not models.Score.objects.filter(c_name=data['c_name'], properties=data['properties']).exists():
-            models.Score.objects.create(stu_id=data['stu_id'], name=data['name'], class_num=data['class_num'],
-                                        major=data['major'], c_name=data['c_name'], term=data['term'],
-                                        c_type=data['c_type'], score=data['score'], credit=data['credit'],
-                                        properties=data['properties'], grade=data['grade'],
-                                        point=data['point'], grade_point=data['grade_point']
-                                        )
-
-def class_rank(request):
-    pass
-
-def index(request):
-    w_data()
-    return render(request, 'index.html', locals())
+    msg = spider.score_spider(username, password)
+    if msg != '':
+        return render(request, 'login.html', {
+            "msg": json.dumps(msg)
+        })
+    datas, years = spider.score_data()
+    sorted(years)
+    print(years)
+    return render(request, 'index.html', {
+        "datas": datas,
+        "years": years
+    })
